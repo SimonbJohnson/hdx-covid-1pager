c1dca002f392436756bba2c52de1e69bee16f10d
SJohnson <simonbjohnson@gmail.com>
2020-10-26 18:34:34 +0000

    update for new data

--- js/site.js
+++ js/site.js
@@ -5,7 +5,7 @@ function loadData(url){
 	    dataType: 'json',
 	    success:function(response){
 	        let dataHXL= hxlProxyToJSON(response);
-	        init(dataHXL,data);
+	        init(dataHXL);
 	    }
 	});
 }
@@ -47,16 +47,16 @@ function init(dataHXLProxy,data){
 
 function prepData(dataHXLProxy,data){
 	let output = dataHXLProxy;
-	output = addColumn(output,data,'national_data','#value+funding+hrp+pct');
+	//output = addColumn(output,data,'national_data','#value+funding+hrp+pct');
 	output.forEach(function(d){
-		if(d['#value+funding+hrp+pct']==undefined){
-			d['#value+funding+hrp+txt'] = '-';
-			d['#value+funding+hrp+pct'] = -1
+		if(d['#value+covid+funding+hrp+pct']==''){
+			d['#value+covid+funding+hrp+txt'] = 'n/a';
+			d['#value+covid+funding+hrp+pct'] = -1
 		} else {
-			d['#value+funding+hrp+txt'] = Math.round(d['#value+funding+hrp+pct']*100)+'%';
+			d['#value+covid+funding+hrp+txt'] = Math.round(d['#value+covid+funding+hrp+pct']*100)+'%';
 		}
 	});
-	output = addColumn(output,data,'national_data','#affected+inneed');
+	//output = addColumn(output,data,'national_data','#affected+inneed');
 	output.forEach(function(d){
 		if(d['#affected+inneed']==undefined){
 			d['#affected+inneed'] = '-';
@@ -64,33 +64,55 @@ function prepData(dataHXLProxy,data){
 			d['#affected+inneed'] = numberWithCommas(d['#affected+inneed']);
 		}
 	});
-	output = addColumn(output,data,'national_data','#affected+tested+per1000');
+	//output = addColumn(output,data,'national_data','#affected+tested+per1000');
 	output.forEach(function(d){
-		if(d['#affected+tested+per1000']==undefined){
-			d['#affected+tested+per1000']= '-'
+		if(d['#affected+pct+positive+tested']==''){
+			d['#affected+pct+positive+tested']= 'n/a'
+		} else {
+			d['#affected+pct+positive+tested'] = Math.round(d['#affected+pct+positive+tested']*100) + '%'
 		}
 	});
-	output = addColumn(output,data,'national_data','#vaccination+num+ratio');
+	//output = addColumn(output,data,'national_data','#vaccination+num+ratio');
 	output.forEach(function(d){
-		if(d['#vaccination+num+ratio']==undefined){
-			d['#vaccination+num+ratio']= '-'
+		if(d['#vaccination+num+ratio']==''){
+			d['#vaccination+num+ratio']= 'n/a'
 		} else {
 			d['#vaccination+num+ratio']= Math.round(d['#vaccination+num+ratio']*100)+'%';
 		}
 	});
-	output = addColumn(output,data,'national_data','#value+food+num+ratio');
+	//output = addColumn(output,data,'national_data','#value+food+num+ratio');
 	output.forEach(function(d){
 		if(d['#value+food+num+ratio']==undefined){
-			d['#value+food+num+ratio']= '-'
+			d['#value+food+num+ratio']= 'n/a'
 		} else {
 			d['#value+food+num+ratio']= Math.round(d['#value+food+num+ratio']*100)+'%';
 		}
+	});
+
+	output.forEach(function(d){
+		if(d['#affected+avg+infected']==undefined){
+			d['#affected+avg+infected'] = 'n/a'
+		} else {
+			d['#affected+avg+infected'] = numberWithCommas(Math.round(d['#affected+avg+infected']));
+		}
+	});
+	
+	output.forEach(function(d){
+		if(d['#affected+avg+killed']==undefined){
+			d['#affected+avg+killed'] = 'n/a'
+		} else {
+			d['#affected+avg+killed'] = numberWithCommas(Math.round(d['#affected+avg+killed']));
+		}
+	});
+
+	output.forEach(function(d){
+		d['#affected+avg+infected+per100000'] = Math.round(d['#affected+avg+infected+per100000']*10)/10
 	});
 	
 	return output;
 }
 
-function addColumn(output,data,rootKey,key){
+/*function addColumn(output,data,rootKey,key){
 	output.forEach(function(d){
 		data[rootKey].forEach(function(d2){
 			if(d['#country+code+v_iso3']==d2['#country+code']){
@@ -99,7 +121,7 @@ function addColumn(output,data,rootKey,key){
 		})
 	});
 	return output;
-};
+};*/
 
 function calcMax(data){
 	config = {}
@@ -117,24 +139,31 @@ function calcMax(data){
 }
 
 function createTable(config,data){
-	barKeys = ['#value+funding+hrp+pct'];
+	barKeys = ['#value+covid+funding+hrp+pct'];
+	data = data.filter(function(d){
+		if(d['#value+covid+funding+hrp+pct']==-1){
+			return false
+		} else {
+			return true
+		}
+	});
 	data = data.sort(function(a,b){
-		return parseFloat(b['#indicator+ht+newcases']) - parseFloat(a['#indicator+ht+newcases']);
+		return parseFloat(b['#affected+avg+infected+per100000']) - parseFloat(a['#affected+avg+infected+per100000']);
 	});
 	console.log(data);
 	console.log(config);
 	data.slice(0,15).forEach(function(d,i){
-		//console.log(d['#country+name']);
 		let html = '<tr><td><span class="index">'+(i+1)+'</span>'+d['#country+name']+'</td>'
-		html += '<td class="rightalign">'+(numberWithCommas(Math.round(d['#indicator+ht+newcases']*10)/10))+'</td><td class="minpadding"><img id="arrow_'+i+'" class="arrow" src="arrow.svg" height="20px"></td>'
-		html += '<td class="rightalign">'+(numberWithCommas(d['#indicator+newcases']))+'</td>'
-		html += '<td class="rightalign">'+(numberWithCommas(d['#indicator+newdeaths']))+'</td><td class="minpadding"><img id="arrow_'+i+'" class="arrow" src="arrow.svg" height="20px"></td>'
-		html += '<td class="rightalign"></td>'
+		html += '<td class="rightalign">'+d['#affected+avg+infected+per100000']+'</td><td class="minpadding"><img id="arrow_'+i+'" class="arrow" src="arrow.svg" height="20px"></td>'
+		html += '<td class="rightalign">'+d['#affected+avg+infected']+'</td>'
+		html += '<td class="rightalign">'+d['#affected+avg+killed']+'</td><td class="minpadding"><img id="arrow2_'+i+'" class="arrow" src="arrow.svg" height="20px"></td>'
+		html += '<td class="rightalign">'+d['#affected+pct+positive+tested']+'</td>'
 		html += '<td class="rightalign">'+d['#vaccination+num+ratio']+'</td>'
 		html += '<td class="rightalign">'+d['#value+food+num+ratio']+'</td>'
-		html += '<td class="rightalign"><div id="bar_0_'+i+'" class="bar"></div><div id="bar_0_grey_'+i+'" class="bar greybar"></div></td><td class="rightalign">'+(d['#value+funding+hrp+txt'])+'</td>'
+		html += '<td class="rightalign"><div id="bar_0_grey_'+i+'" class="bar greybar"></div><div id="bar_0_'+i+'" class="bar"></div></td><td class="rightalign">'+(d['#value+covid+funding+hrp+txt'])+'</td>'
 		html += '</tr>';
 		$('#maintable').append(html);
+
 		barKeys.forEach(function(k,j){
 			if(d[k]!=-1){
 				let id = '#bar_'+(j)+'_'+i;
@@ -145,7 +174,8 @@ function createTable(config,data){
 				$(greyID).width(greyWidth);
 			}
 		});
-		let rotate = d['#indicator+newcaseschange']/10*-45;
+
+		let rotate = d['#affected+avg+change+infected+pct+per100000']/10*-45;
 		if(rotate<-45){
 			rotate = -45
 		}
@@ -155,7 +185,20 @@ function createTable(config,data){
 		$('#arrow_'+i).css({
 	        "-webkit-transform": "rotate("+rotate+"deg)",
 	        "-moz-transform": "rotate("+rotate+"deg)",
-	        "transform": "rotate("+rotate+"deg)" /* For modern browsers(CSS3)  */
+	        "transform": "rotate("+rotate+"deg)"
+    	});
+
+    	rotate = d['#affected+avg+change+killed+pct']/10*-45;
+		if(rotate<-45){
+			rotate = -45
+		}
+		if(rotate>45){
+			rotate = 45
+		}
+		$('#arrow2_'+i).css({
+	        "-webkit-transform": "rotate("+rotate+"deg)",
+	        "-moz-transform": "rotate("+rotate+"deg)",
+	        "transform": "rotate("+rotate+"deg)"
     	});
 	});
 }
@@ -166,6 +209,6 @@ function numberWithCommas(x) {
 
 //load 3W data
 
-let url = 'https://proxy.hxlstandard.org/data.json?dest=data_edit&filter01=select&select-query01-01=%23date%2Breported+is+max&filter02=cut&cut-skip-untagged02=on&filter03=merge&merge-url03=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F15L8loGDds69jzAb0v8H-o4Kkc_JIiszxmmP12g8S2iU%2Fedit%23gid%3D0&merge-keys03=%23country%2Bcode&merge-tags03=%23country%2Bname&strip-headers=on&url=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F190B8HBS9j2z1kxxpmYSm8p_zS16SXbljqJFa2t3ljHk%2Fedit%3Fusp%3Dsharing';
+let url = 'https://proxy.hxlstandard.org/data.json?dest=data_edit&filter01=select&select-query01-01=%23date+is+max&filter02=merge&merge-url02=https%3A%2F%2Fproxy.hxlstandard.org%2Fdata.csv%3Fdest%3Ddata_view%26url%3Dhttps%253A%252F%252Fraw.githubusercontent.com%252FOCHA-DAP%252Fhdx-scraper-covid-viz%252Fmaster%252Fout_daily.json%26selector%3Dcumulative&merge-keys02=%23country%2Bcode&merge-tags02=%23affected%2C%23value%2C%23vaccination&strip-headers=on&url=https%3A%2F%2Fraw.githubusercontent.com%2FOCHA-DAP%2Fhdx-scraper-covid-viz%2Fmaster%2Fout_daily.json';
 
 loadData(url);
